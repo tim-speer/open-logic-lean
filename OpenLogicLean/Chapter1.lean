4e69abb0d4922d7aa47f2a00d2c8069645aba7e5
Tim Speer <94493935+tim-speer@users.noreply.github.com>
2025-06-15 09:50:46 -0700

    Started work on Chapter 1 of Open Logic

---
+++ OpenLogicLean/Chapter1.lean
@@ -0,0 +1,51 @@
+import Mathlib.Data.Set.Basic
+
+-----------------
+-- Section 1.1 --
+-----------------
+section Section_1_1
+
+-- Definition 1.1 --
+def set_equality {α : Type} (A B : Set α) : Prop :=
+  ∀ x, x ∈ A ↔ x ∈ B
+
+end Section_1_1
+
+
+-----------------
+-- Section 1.2 --
+-----------------
+section Section_1_2
+
+-- Definition 1.5 --
+def subset {α : Type} (A B : Set α) : Prop :=
+  ∀ x, x ∈ A → x ∈ B
+
+def proper_subset {α : Type} (A B : Set α) : Prop :=
+  subset A B ∧ ¬ set_equality A B
+
+-- Example 1.6 --
+theorem subset_self {α : Type} (A : Set α) : subset A A := by
+  rw [subset]
+  intro x x_in_A
+  exact x_in_A
+
+-- Proposition 1.8 --
+theorem Proposition_1_8 {α : Type} (A B : Set α) :
+set_equality A B ↔ subset A B ∧ subset B A := by
+  rw [set_equality, subset, subset]
+  constructor
+  . intro A_equals_B
+    constructor
+    . intro x x_in_A
+      apply (A_equals_B x).mp x_in_A
+    . intro x x_in_B
+      apply (A_equals_B x).mpr x_in_B
+  . intro ⟨A_subset_B, B_subset_A⟩ x
+    constructor
+    . intro x_in_A
+      exact A_subset_B x x_in_A
+    . intro x_in_B
+      exact B_subset_A x x_in_B
+
+end Section_1_2
