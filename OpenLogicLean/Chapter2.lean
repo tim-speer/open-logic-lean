449c52321b14f469a85061c9bd6ac749fa5f370b
Tim Speer <94493935+tim-speer@users.noreply.github.com>
2025-06-23 21:28:25 -0700

    Added equivalence relation definition and updated relation properties

--- OpenLogicLean/Chapter2.lean
+++ OpenLogicLean/Chapter2.lean
@@ -21,44 +21,58 @@ Section 2.3
 section Section_2_3
 
 -- Definition 2.3 --
-def reflexive {α : Type} (R : Set (Set (Set α))) (A : Set α)
-  (h : binary_relation R A) : Prop :=
-  ∀ x, x ∈ A → binary_relation R A → (ordered_pair x x) ∈ R
+def reflexive {α : Type} (R : Set (Set (Set α))) (A : Set α) : Prop :=
+  binary_relation R A ∧
+  (∀ x, x ∈ A → binary_relation R A → (ordered_pair x x) ∈ R)
 
 -- Definition 2.4 --
-def transitive {α : Type} (R : Set (Set (Set α))) (A : Set α)
-  (h : binary_relation R A) : Prop :=
-  ∀ x y z, x ∈ A ∧ y ∈ A ∧ z ∈ A →
+def transitive {α : Type} (R : Set (Set (Set α))) (A : Set α) : Prop :=
+  binary_relation R A ∧
+  (∀ x y z, x ∈ A ∧ y ∈ A ∧ z ∈ A →
   (ordered_pair x y) ∈ R ∧ (ordered_pair y z) ∈ R →
-  (ordered_pair x z) ∈ R
+  (ordered_pair x z) ∈ R)
 
 -- Definition 2.5 --
-def symmetric {α : Type} (R : Set (Set (Set α))) (A : Set α)
-  (h : binary_relation R A) : Prop :=
-  ∀ x y , x ∈ A ∧ y ∈ A → (ordered_pair x y) ∈ R →
-  (ordered_pair y x ∈ R)
+def symmetric {α : Type} (R : Set (Set (Set α))) (A : Set α) : Prop :=
+  binary_relation R A ∧
+  (∀ x y , x ∈ A ∧ y ∈ A → (ordered_pair x y) ∈ R →
+  (ordered_pair y x ∈ R))
 
 -- Definition 2.6 --
 def anti_symmetric {α : Type} (R : Set (Set (Set α))) (A : Set α)
-  (h : binary_relation R A) : Prop :=
-  ∀ x y, x ∈ A ∧ y ∈ A → (ordered_pair x y) ∈ R ∧ (ordered_pair y x ∈ R)
-  → x = y
+  : Prop :=
+  binary_relation R A ∧
+  (∀ x y, x ∈ A ∧ y ∈ A → (ordered_pair x y) ∈ R ∧ (ordered_pair y x ∈ R)
+  → x = y)
 
 -- Definition 2.7 --
-def connected {α : Type} (R : Set (Set (Set α))) (A : Set α)
-  (h : binary_relation R A) : Prop :=
-  ∀ x y, x ∈ A ∧ y ∈ A → x ≠ y →
-  (ordered_pair x y) ∈ R ∨ (ordered_pair y x) ∈ R
+def connected {α : Type} (R : Set (Set (Set α))) (A : Set α) : Prop :=
+  binary_relation R A ∧
+  (∀ x y, x ∈ A ∧ y ∈ A → x ≠ y →
+  (ordered_pair x y) ∈ R ∨ (ordered_pair y x) ∈ R)
 
 -- Definition 2.8 --
-def irreflexive {α : Type} (R : Set (Set (Set α))) (A : Set α)
-  (h : binary_relation R A) : Prop :=
-  ∀ x, x ∈ A → (ordered_pair x x) ∉ R
+def irreflexive {α : Type} (R : Set (Set (Set α))) (A : Set α) : Prop :=
+  binary_relation R A ∧
+  (∀ x, x ∈ A → (ordered_pair x x) ∉ R)
 
 -- Definition 2.9 --
-def asymmetric {α : Type} (R : Set (Set (Set α))) (A : Set α)
-  (h : binary_relation R A) : Prop :=
-  ∀ x y, x ∈ A ∧ y ∈ A → x ≠ y →
-  (ordered_pair x y) ∉ R ∨ (ordered_pair y x) ∉ R
+def asymmetric {α : Type} (R : Set (Set (Set α))) (A : Set α) : Prop :=
+  binary_relation R A ∧
+  (∀ x y, x ∈ A ∧ y ∈ A → x ≠ y →
+  (ordered_pair x y) ∉ R ∨ (ordered_pair y x) ∉ R)
 
 end Section_2_3
+
+
+/-*********
+Section 2.4
+*********-/
+section Section_2_4
+
+-- Definition 2.10 --
+def equivalence_relation {α : Type}
+  (R : Set (Set (Set α))) (A : Set α) : Prop :=
+  reflexive R A ∧ symmetric R A ∧ transitive R A
+
+end Section_2_4
