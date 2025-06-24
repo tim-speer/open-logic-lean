f2fb3f30c48dde7f1d680cb073d904c3b0051479
Tim Speer <94493935+tim-speer@users.noreply.github.com>
2025-06-24 13:13:36 -0700

    Added binary relation structure and simplified binary relation property definitions

--- OpenLogicLean/Chapter2.lean
+++ OpenLogicLean/Chapter2.lean
@@ -8,9 +8,15 @@ Section 2.1
 section Section_2_1
 
 -- Definition 2.1 --
-def binary_relation {α : Type}
+def is_binary_relation {α : Type}
   (R : Set (Set (Set α))) (A : Set α) : Prop :=
   subset R (cartesian_product A A)
+
+structure binary_relation where
+  α : Type
+  A : Set α
+  R : Set (Set (Set α))
+  br : is_binary_relation R A
 
 end Section_2_1
 
@@ -21,46 +27,38 @@ Section 2.3
 section Section_2_3
 
 -- Definition 2.3 --
-def reflexive {α : Type} (R : Set (Set (Set α))) (A : Set α) : Prop :=
-  binary_relation R A ∧
-  (∀ x, x ∈ A → binary_relation R A → (ordered_pair x x) ∈ R)
+def reflexive (bin_rel : binary_relation) : Prop :=
+  ∀ x, x ∈ bin_rel.A → (ordered_pair x x) ∈ bin_rel.R
 
 -- Definition 2.4 --
-def transitive {α : Type} (R : Set (Set (Set α))) (A : Set α) : Prop :=
-  binary_relation R A ∧
-  (∀ x y z, x ∈ A ∧ y ∈ A ∧ z ∈ A →
-  (ordered_pair x y) ∈ R ∧ (ordered_pair y z) ∈ R →
-  (ordered_pair x z) ∈ R)
+def transitive (bin_rel : binary_relation) : Prop :=
+  ∀ x y z, x ∈ bin_rel.A ∧ y ∈ bin_rel.A ∧ z ∈ bin_rel.A →
+  (ordered_pair x y) ∈ bin_rel.R ∧ (ordered_pair y z) ∈ bin_rel.R →
+  (ordered_pair x z) ∈ bin_rel.R
 
 -- Definition 2.5 --
-def symmetric {α : Type} (R : Set (Set (Set α))) (A : Set α) : Prop :=
-  binary_relation R A ∧
-  (∀ x y , x ∈ A ∧ y ∈ A → (ordered_pair x y) ∈ R →
-  (ordered_pair y x ∈ R))
+def symmetric (bin_rel : binary_relation) : Prop :=
+  ∀ x y, x ∈ bin_rel.A ∧ y ∈ bin_rel.A → (ordered_pair x y) ∈ bin_rel.R →
+  (ordered_pair y x ∈ bin_rel.R)
 
 -- Definition 2.6 --
-def anti_symmetric {α : Type} (R : Set (Set (Set α))) (A : Set α)
-  : Prop :=
-  binary_relation R A ∧
-  (∀ x y, x ∈ A ∧ y ∈ A → (ordered_pair x y) ∈ R ∧ (ordered_pair y x ∈ R)
-  → x = y)
+def anti_symmetric (bin_rel : binary_relation) : Prop :=
+  ∀ x y, x ∈ bin_rel.A ∧ y ∈ bin_rel.A →
+  (ordered_pair x y) ∈ bin_rel.R ∧ (ordered_pair y x ∈ bin_rel.R) → x = y
 
 -- Definition 2.7 --
-def connected {α : Type} (R : Set (Set (Set α))) (A : Set α) : Prop :=
-  binary_relation R A ∧
-  (∀ x y, x ∈ A ∧ y ∈ A → x ≠ y →
-  (ordered_pair x y) ∈ R ∨ (ordered_pair y x) ∈ R)
+def connected (bin_rel : binary_relation) : Prop :=
+  ∀ x y, x ∈ bin_rel.A ∧ y ∈ bin_rel.A → x ≠ y →
+  (ordered_pair x y) ∈ bin_rel.R ∨ (ordered_pair y x) ∈ bin_rel.R
 
 -- Definition 2.8 --
-def irreflexive {α : Type} (R : Set (Set (Set α))) (A : Set α) : Prop :=
-  binary_relation R A ∧
-  (∀ x, x ∈ A → (ordered_pair x x) ∉ R)
+def irreflexive (bin_rel : binary_relation) : Prop :=
+  ∀ x, x ∈ bin_rel.A → (ordered_pair x x) ∉ bin_rel.R
 
 -- Definition 2.9 --
-def asymmetric {α : Type} (R : Set (Set (Set α))) (A : Set α) : Prop :=
-  binary_relation R A ∧
-  (∀ x y, x ∈ A ∧ y ∈ A → x ≠ y →
-  (ordered_pair x y) ∉ R ∨ (ordered_pair y x) ∉ R)
+def asymmetric (bin_rel : binary_relation) : Prop :=
+  ∀ x y, x ∈ bin_rel.A ∧ y ∈ bin_rel.A → x ≠ y →
+  (ordered_pair x y) ∉ bin_rel.R ∨ (ordered_pair y x) ∉ bin_rel.R
 
 end Section_2_3
 
